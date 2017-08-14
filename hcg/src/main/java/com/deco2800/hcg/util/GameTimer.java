a8b415d1f0981f504492e9b5f8bf2d281d7492ed
Steven Khoupongsy <stkontheweb@gmail.com>
2017-08-14 21:09:52 +1000

    Here begins work on game timing functions. Branch my pretties

---
+++ hcg/src/main/java/com/deco2800/hcg/util/GameTimer.java
@@ -0,0 +1,11 @@
+package com.deco2800.hcg.util;
+
+import java.util.Optional;
+
+/**
+ * A utility class for the Game Timer
+ * Created by Group 2 (guys what's our name) on 14/817.
+ */
+public class GameTimer {
+
+}
