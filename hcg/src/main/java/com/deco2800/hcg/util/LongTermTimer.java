015519ba884f7247372a3e56be0502ac2c660908
Steven Khoupongsy <stkontheweb@gmail.com>
2017-08-14 21:16:10 +1000

    Added LongTermTimer for functions related to accumulation of game time. (Currently empty)

---
+++ hcg/src/main/java/com/deco2800/hcg/util/LongTermTimer.java
@@ -0,0 +1,15 @@
+package com.deco2800.hcg.util;
+
+import java.util.Optional;
+
+/**
+ * A utility class for the Long Term Game time. 
+ * 
+ * i.e. Mechanisms to store an accumulation of time in units 
+ * such as days/months/years.
+ * 
+ * Created by Group 2 (guys what's our name) on 14/817.
+ */
+public class LongTermTimer {
+	
+}
