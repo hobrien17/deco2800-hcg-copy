dd288c6217ef9ae99e33617940b9fcc5c28705e0
Steven Khoupongsy <stkontheweb@gmail.com>
2017-08-15 22:33:09 +1000

    Added some verrry simple methods that calculate and get weeks since time start, and 'simple' months (i.e. months of 4 week blocks, not calendar months).

--- hcg/src/main/java/com/deco2800/hcg/util/CalendarManager.java
+++ hcg/src/main/java/com/deco2800/hcg/util/CalendarManager.java
@@ -48,5 +48,28 @@ public class CalendarManager implements Tickable {
 		return hour;
 	}
 	
+	/**
+	 * getter method for weeks since start
+	 * A week is defined as 7 days (duh)
+	 * @return weeks from start (int) 
+	 */
+	public int getWeeksSinceStart() {
+		int currentDay = this.getDay();
+		// integer div is wizardry I tell you
+		return currentDay/7;
+	}	
+	
+	/**
+	 * getter method for 'simple' months past start month
+	 * For this method, months are defined in chunks 
+	 * of 4 weeks, as opposed to 28/29/30/31 days,
+	 * hence the simple moniker. 
+	 * @return months from start (int) 
+	 */
+	public int getSimpleMonthsSinceStart() {
+		int currentWeek = this.getWeeksSinceStart();
+		return currentWeek/4;
+	}
+	
 	
 }
