bd25ce21096b2767c4e739e88bb85760f0c05275
AnnaTruffet <anna_truffet@hotmail.com>
2017-08-15 17:39:59 +1000

    Added getter methods and on tick method

--- hcg/src/main/java/com/deco2800/hcg/util/CalendarManager.java
+++ hcg/src/main/java/com/deco2800/hcg/util/CalendarManager.java
@@ -13,9 +13,40 @@ import com.deco2800.hcg.entities.Tickable;
  */
 
 public class CalendarManager implements Tickable {
+	// current day
+	private int day;
+	
+	// current hour
+	private int hour;
+	
+	public CalendarManager(){
+		day = 0;
+		hour = 0;
+	}
 	
 	@Override
 	public void onTick(long gameTickCount) {
-		// pass
+		if(++hour >= 24){
+			day++;
+			hour = 0;
+		}
 	}
+	
+	/**
+	 * getter method for current day 
+	 * @return current Day (int) 
+	 */
+	public int getDay(){
+		return day;
+	}
+	
+	/**
+	 * getter method for current hour 
+	 * @return current hour (int) 
+	 */
+	public int getHour(){
+		return hour;
+	}
+	
+	
 }
