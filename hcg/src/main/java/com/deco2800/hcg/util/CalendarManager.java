4b36ac483124cf13880611271c2f7142b45ca4b1
Steven Khoupongsy <stkontheweb@gmail.com>
2017-08-18 22:14:29 +1000

    Added functionality to count months passed according to calendar months. Dunno if actually needed but it's there if needed.

--- hcg/src/main/java/com/deco2800/hcg/util/CalendarManager.java
+++ hcg/src/main/java/com/deco2800/hcg/util/CalendarManager.java
@@ -19,7 +19,7 @@ public class CalendarManager implements Tickable {
 
 	// current day
 	private int day;
-	
+
 	// current day in year
 	private int yearDay;
 
@@ -34,11 +34,14 @@ public class CalendarManager implements Tickable {
 	private int currentYear = initialYear;
 
 	// initial month - same deal as initial year.
+	// however, I would recommend leaving it at 0/Jan, to avoid
+	// overly complex code
 	// In this case months will be 0-11 corresponding with Jan-Dec.
 	private int initialMonth = 0;
+	private int currentMonth = initialMonth;
 	// days in months
-	private int[] dayCount = {31,28,31,30,31,30,31,31,30,31,30,31};
-	private int[] dayCountLeapYear = {31,29,31,30,31,30,31,31,30,31,30,31};
+	private int[] dayCount = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private int[] dayCountLeapYear = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public CalendarManager() {
 		day = 0;
@@ -46,15 +49,19 @@ public class CalendarManager implements Tickable {
 	}
 
 	@Override
+	// at a tickrate of 50Hz, we might need to do some ~mathematics~ here?
+	// cause if my understanding is correct, we'll end up going 50 hours a second
+	// lol
 	public void onTick(long gameTickCount) {
 		if (++hour >= 24) {
 			day++;
 			yearDay++;
 			hour = 0;
 		}
-		
+
+		incrementCalendarMonths();
 		incrementYear();
-			
+
 	}
 
 	/*
@@ -74,7 +81,7 @@ public class CalendarManager implements Tickable {
 		}
 		return false;
 	}
-	
+
 	/*
 	 * Helper method, increments year with conditions
 	 * 
@@ -83,7 +90,7 @@ public class CalendarManager implements Tickable {
 	 */
 	private void incrementYear() {
 		if (yearDay >= 365) {
-			if(!this.isLeapYear()) {
+			if (!this.isLeapYear()) {
 				currentYear++;
 				yearDay = 0;
 			} else {
@@ -94,6 +101,30 @@ public class CalendarManager implements Tickable {
 			}
 		}
 	}
+	
+	/*
+	 * helper method to increment months
+	 */
+	private void incrementCalendarMonths() {
+		int bufferDay = yearDay;
+		int currentMonth = this.getCurrentCalendarMonth();
+		int dayCounter = 0;
+		
+		if(this.isLeapYear()) {
+			for (int i = 0; i < currentMonth; i++) {
+				dayCounter = dayCounter + dayCountLeapYear[i];
+			}
+		} else {
+			for (int i = 0; i < currentMonth; i++) {
+				dayCounter = dayCounter + dayCount[i];
+			}
+		}
+		
+		if (bufferDay >= dayCounter) {
+			currentMonth++;
+		}	
+	}
+	
 
 	/**
 	 * getter method for current day
@@ -148,34 +179,36 @@ public class CalendarManager implements Tickable {
 	/**
 	 * getter method to get what calendar month it is
 	 * 
-	 * @return calendar month 0-11 (int), if an error has 
-	 * 	occurred, 999 will be returned.
+	 * @return calendar month 0-11 (int), if an error has occurred, 999 will be
+	 *         returned.
 	 */
 	public int getCurrentCalendarMonth() {
-		
-		// setting up buffer variables so I don't 
+
+		// setting up buffer variables so I don't
 		// accidentally f-up the proper ones
 		int dayBuffer = yearDay;
-		int dayCounter = -1; // to account for 
-		
+		int dayCounter = -1; // to account for
+
 		if (!this.isLeapYear()) {
-			for (int i=0;i<dayCountLeapYear.length;i++) {
+			for (int i = 0; i < dayCountLeapYear.length; i++) {
 				dayCounter = dayCounter + dayCountLeapYear[i];
 				if (dayCounter >= dayBuffer) {
 					return i;
-				} 
+				}
 			}
 		} else {
-			for (int i=0;i<dayCount.length;i++) {
+			for (int i = 0; i < dayCount.length; i++) {
 				dayCounter = dayCounter + dayCount[i];
 				if (dayCounter >= dayBuffer) {
 					return i;
-				} 
+				}
 			}
 		}
-		
+
 		return 999;
-		
+
 	}
+	
+
 
 }
