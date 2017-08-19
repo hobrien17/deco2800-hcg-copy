b16cc1c078b1ddf5e52bc72b0b859fd41e397a3a
Henry O'Brien <h.obrien17@hotmail.com>
2017-08-19 16:38:36 +1000

    Added SunflowerSeed seed type

--- hcg/src/main/java/com/deco2800/hcg/entities/garden_entities/seeds/AbstractSeed.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/garden_entities/seeds/AbstractSeed.java
@@ -1,5 +1,6 @@
 package com.deco2800.hcg.entities.garden_entities.seeds;
 
+import com.deco2800.hcg.entities.garden_entities.plants.AbstractGardenPlant;
 import com.deco2800.hcg.items.Item;
 
 /**
@@ -15,4 +16,28 @@ public abstract class AbstractSeed implements Item {
     public boolean isStackable() {
     	return true;
     }
+    
+    @Override
+	public boolean isWearable() {
+		return false;
+	}
+    
+    @Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+    
+    @Override
+	public int maxStackSize() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+    
+    /**
+     * Gets the type of plant that grows when planted
+     * 
+     * @return the type of plant that grows out of this seed
+     */
+    public abstract AbstractGardenPlant plant();
 }
