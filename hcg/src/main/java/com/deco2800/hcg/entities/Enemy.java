04e61d32b9d0c7ca452d7eb87a04398567aa613f
Grace Rehn <grace_rehn@hotmail.com>
2017-08-21 13:53:24 +1000

    Added attack damage

--- hcg/src/main/java/com/deco2800/hcg/entities/Enemy.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/Enemy.java
@@ -3,17 +3,22 @@ package com.deco2800.hcg.entities;
 import com.deco2800.hcg.util.Box3D;
 
 public abstract class Enemy extends Character implements Harmable {
-    //private int health;
+    private int attack;
 
     public Enemy(float posX, float posY, float posZ, float xLength, float yLength, float zLength, boolean centered,
-                 int health) {
+                   int health, int attack) {
         super(posX, posY, posZ, xLength, yLength, zLength, centered);
         if (health > 0) {
             this.health = health;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException();
         }
+        if (attack > 0) {
+            this.attack = attack;
+        } else {
+            throw new IllegalArgumentException();
+        }
+
     }
 
     //public Enemy(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
@@ -25,18 +30,16 @@ public abstract class Enemy extends Character implements Harmable {
     //    super(position, xRenderLength, yRenderLength, centered);
     //}
     
-    //public int getHealth() {
-    //    return this.health;
-    //}
-    
-    //private void setHealth(int newHealth) {
-    //    if (newHealth > 0) {
-    //        this.health = newHealth;
-    //    }
-    //    else {
-    //        throw new IllegalArgumentException();
-    //    }
-    //}
+
+    private void setAttack(int attackDamage) {
+        if (attackDamage < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            attack = attackDamage;
+        }
+    }
+
+    public int getAttack() {return attack;}
 
     public void takeDamage(int damage) {
         if (damage < 0){
