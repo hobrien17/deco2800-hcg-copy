dd2b5320be26aad22b2ab5d3ecfb669f78ecd7e1
Lilly (Li) Tran <lillytr23@outlook.com>
2017-08-22 13:20:31 +1000

    tidy up some methods

--- hcg/src/main/java/com/deco2800/hcg/entities/Enemy.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/Enemy.java
@@ -1,36 +1,36 @@
 package com.deco2800.hcg.entities;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.deco2800.hcg.util.Box3D;
 
 public abstract class Enemy extends Character implements Harmable {
-    private int attack;
-
+    
+    // logger for this class
+    private static final Logger LOGGER = LoggerFactory.getLogger(Enemy.class);
+    
+    
     public Enemy(float posX, float posY, float posZ, float xLength, float yLength, float zLength, boolean centered,
-                   int health, int attack) {
+                   int health, int strength) {
         super(posX, posY, posZ, xLength, yLength, zLength, centered);
         if (health > 0) {
             this.health = health;
         } else {
             throw new IllegalArgumentException();
         }
-        if (attack > 0) {
-            this.attack = attack;
+        if (strength > 0) {
+            this.strength = strength;
         } else {
             throw new IllegalArgumentException();
         }
-
     }
-
-    //public Enemy(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
-    //                      float xRenderLength, float yRenderLength, boolean centered) {
-    //   super(posX, posY, posZ, xLength, yLength, zLength, xRenderLength, yRenderLength, centered);
-    //}
 
     //public Enemy(Box3D position, float xRenderLength, float yRenderLength, boolean centered) {
     //    super(position, xRenderLength, yRenderLength, centered);
     //}
     
-
+    /*
     private void setAttack(int attackDamage) {
         if (attackDamage < 0) {
             throw new IllegalArgumentException();
@@ -40,7 +40,14 @@ public abstract class Enemy extends Character implements Harmable {
     }
 
     public int getAttack() {return attack;}
+    */
 
+    /**
+     * Take the damage inflicted by the other entities
+     * 
+     * @param damage: the amount of the damage
+     * @exception: throw IllegalArgumentException if damage is a negative integer
+     */
     public void takeDamage(int damage) {
         if (damage < 0){
             throw new IllegalArgumentException();
@@ -53,17 +60,17 @@ public abstract class Enemy extends Character implements Harmable {
         }
     }
 
-    public void gainHealth(int extraHealth) {
-        if (extraHealth < 0){
-            throw new IllegalArgumentException();
-        }
-        else {
-            health += extraHealth;
-        }
+    /**
+     * Change the health level of the enemy i.e can increase or decrease the health level (depending on whether the amount is positive or negative)
+     * 
+     * @param amount: the amount that the health level will change by
+     */
+    public void changeHealth(int amount) {
+        health = Math.max(health + amount, 0);
     }
 
     // attack player
-    public void causeHarm(){
-
+    public void causeDamage(Player player){
+        
     }
 }
