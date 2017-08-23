4b120a17625b182f447a2639ab655204430b6a40
Harry Guthrie <GTGuthers@gmail.com>
2017-08-23 15:19:18 +1000

    Additional NPC info

--- hcg/src/main/java/com/deco2800/hcg/entities/NPC.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/NPC.java
@@ -12,22 +12,53 @@ package com.deco2800.hcg.entities;
  *
  * @author guthers
  */
-public abstract class NPC extends Character {
-    public String Fname;
-    public String SName;
+public abstract class NPC extends Character implements Clickable {
+    public enum Type {
+        Shop,
+        Quest
+    }
 
-
-
-
-
-
+    public String fName;
+    public String sName;
+    public NPC.Type NPCType;
 
     public NPC(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
-               boolean centered) {
+               boolean centered,String fName,String sName,NPC.Type NPCType) {
+
+        //Set up the parent constructor
         super(posX,posY,posZ,xLength,yLength,zLength,centered);
+
+        //Set up the new NPC
+        this.fName = fName;
+        this.sName = sName;
+        this.NPCType = NPCType;
+
+        //We want specific NPCs to have set speeds
+        if (NPCType == Type.Shop) {
+            this.movementSpeed = 0;
+        } else if (NPCType == Type.Quest) {
+            this.movementSpeed = 0.01f;
+        } else {
+            //Set all other NPCs to some default speed
+            this.movementSpeed = 0.02f;
+        }
 
     }
 
+    private void interact() {
+        //What do we do with this NPC on click?
+        if (NPCType == Type.Shop) {
+            //Open Shop Menu
+        } else if (NPCType == Type.Quest) {
+            //Open Quest Menu
+        } else {
+            //Do we even do anything?
+        }
+    }
+
+    //Currently being used of a way to interact with NPCs
+    @Override
+    public void onClick() {
+        interact();
+    }
 }
-
-
