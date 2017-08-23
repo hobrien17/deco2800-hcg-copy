70ff689d5a3a7d08a3cd751385e5f6417f06a224
AnnaTruffet <anna_truffet@hotmail.com>
2017-08-23 13:45:03 +1000

    added managers test package and added a class for timemanager testing

---
+++ hcg/src/test/java/com/deco2800/hcg/managers/timeManagerTests.java
@@ -0,0 +1,13 @@
+package com.deco2800.hcg.managers;
+
+import org.junit.*;
+
+public class timeManagerTests {
+	private TimeManager timeManager;
+
+	public timeManagerTests() throws Exception {
+		timeManager = new TimeManager();
+
+	}
+
+}
