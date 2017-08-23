a65012c4003146d127c83f82eac52b1f6faa1229
AnnaTruffet <anna_truffet@hotmail.com>
2017-08-23 14:13:02 +1000

    added isLeapYear test

--- hcg/src/test/java/com/deco2800/hcg/managers/timeManagerTests.java
+++ hcg/src/test/java/com/deco2800/hcg/managers/timeManagerTests.java
@@ -5,9 +5,27 @@ import org.junit.*;
 public class timeManagerTests {
 	private TimeManager timeManager;
 
-	public timeManagerTests() throws Exception {
+	@Before
+	public void setUp() {
 		timeManager = new TimeManager();
-
 	}
 
+	@Test
+	public void isLeapYearTest() throws Exception {
+		// not leap year
+		timeManager.setYear(1999);
+		Assert.assertFalse(timeManager.isLeapYear());
+
+		// is leap year
+		timeManager.setYear(2016);
+		Assert.assertTrue(timeManager.isLeapYear());
+
+		// not leap year (because divisible by 100)
+		timeManager.setYear(1900);
+		Assert.assertFalse(timeManager.isLeapYear());
+
+		// not leap year (because divisible by 400)
+		timeManager.setYear(2000);
+		Assert.assertTrue(timeManager.isLeapYear());
+	}
 }
