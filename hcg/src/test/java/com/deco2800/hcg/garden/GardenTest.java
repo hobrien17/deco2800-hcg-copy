589410c070de5f03a1da692ed6ebd37eef841cff
Henry O'Brien <h.obrien17@hotmail.com>
2017-08-26 12:39:38 +1000

    Added temporary sprites to unimplemented plants

--- hcg/src/test/java/com/deco2800/hcg/garden/GardenTest.java
+++ hcg/src/test/java/com/deco2800/hcg/garden/GardenTest.java
@@ -11,7 +11,9 @@ import org.junit.Test;
 import com.deco2800.hcg.entities.Player;
 import com.deco2800.hcg.entities.garden_entities.plants.AbstractGardenPlant;
 import com.deco2800.hcg.entities.garden_entities.plants.Cactus;
+import com.deco2800.hcg.entities.garden_entities.plants.Explosive;
 import com.deco2800.hcg.entities.garden_entities.plants.Grass;
+import com.deco2800.hcg.entities.garden_entities.plants.Ice;
 import com.deco2800.hcg.entities.garden_entities.plants.Inferno;
 import com.deco2800.hcg.entities.garden_entities.plants.Planter;
 import com.deco2800.hcg.entities.garden_entities.plants.Pot;
@@ -34,6 +36,8 @@ public class GardenTest {
 		sprites.put(Grass.class, "grass");
 		sprites.put(Water.class, "lily");
 		sprites.put(Inferno.class, "inferno");
+		sprites.put(Ice.class, "sunflower"); //need to change
+		sprites.put(Explosive.class, "sunflower"); //need to change
 	}
 
 	@Test
@@ -61,6 +65,7 @@ public class GardenTest {
 		AbstractGardenPlant plant = new Sunflower(p);
 		assertTrue(p.addPlant(plant));
 		assertEquals(p.getPlant(), plant);
+		assertEquals(plant.getGrowDelay(), 10);
 	}
 	
 	@Test
@@ -75,7 +80,8 @@ public class GardenTest {
 	@Test
 	public void testAllPlants() {
 		Pot p = new Pot(5, 5, 0);
-		AbstractGardenPlant[] plants = {new Sunflower(p), new Cactus(p), new Water(p), new Grass(p), new Inferno(p)};
+		AbstractGardenPlant[] plants = {new Sunflower(p), new Cactus(p), new Water(p), new Grass(p), new Inferno(p),
+				new Ice(p), new Explosive(p)};
 		for(AbstractGardenPlant plant : plants) {
 			p.removePlant();
 			p.addPlant(plant);
