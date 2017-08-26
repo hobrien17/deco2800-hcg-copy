8fdfd868e5831edb09bbb9f82effb43d51e6292b
coppers-j <james.copperthwaite@googlemail.com>
2017-08-26 18:46:01 +1000

    changed items

--- hcg/src/main/java/com/deco2800/hcg/items/Item.java
+++ hcg/src/main/java/com/deco2800/hcg/items/Item.java
@@ -1,6 +1,9 @@
 package com.deco2800.hcg.items;
 
-/** Item interface provides a high level guide for others to implement custom items.
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+/**
+ * Item interface provides a high level guide for others to implement custom items.
  * The following methods outline the most basic methods an item requires
  * See the basicItem java class for an example implementation of this interface. Not that simple items are not stackable.
  * For items that are required to be stacked, use the StackableItem interface.
@@ -17,21 +20,45 @@ package com.deco2800.hcg.items;
  * isWearable: A simple boolean return value representing whether the item can be equipped/worn by a user. These
  *              items dont necessarily need to provide damange protection to the character and could simply be a
  *              customization item.
+ * @authors coppers-j, willcs, raymogg
  **/
 public interface Item {
     /**Each item has four required fields: name, isStackable (a boolean value), itemWeight and itemIcon **/
 
-    /** Function for returning the name of an item **/
+    /**
+     * Retrieves an items display name
+     * @return Name of item as String
+     */
     String getName();
 
-    /**Function for getting the max stack size. 1 indicates a non stackable item */
+    /**
+     * Retrieves the stack limit of item type
+     * @return Maximum stack size of item
+     */
     int getMaxStackSize();
 
-    /** Function for returning whether an item is wearable/equipable by a user
-     * eg armour, character customization items */
+    /**
+     * Checks if the item is armour or character customisation item
+     * @return Whether or not item is wearable
+     */
     boolean isWearable();
 
-    /** Function for returning the total weight of this item. For stackable items this is equal to itemWeight * stackSize **/
+    /**
+     * Checks if item is a weapon/ potion etc. and can be held in hot bar
+     * @return Whether or not item can be equipped in hot bar
+     */
+    boolean isEquippable();
+
+    /**
+     * Checks if max stack size is 1
+     * @return Whether or not item will stack in inventory
+     */
+    boolean isStackable();
+
+    /**
+     * Retrieves the total weight of current stack of items
+     * @return Item weight multiplied by current stack size
+     */
     int getWeight();
     
     /**
@@ -46,16 +73,22 @@ public interface Item {
      */
     boolean isTradable();
 
-    /** Function for setting the icon of an item
+    /**
+     * Function for setting the icon of an item
      * Implemented similar to the AbstractEntitry texture. Be sure to register texture with
-     * TextureRegister before assigning the texture to a item*/
-    void setTexture(String texture);
+     * TextureRegister before assigning the texture to a item
+     * @param texture: filename of texture
+     * @throws IllegalArgumentException if texture is an invalid file name
+     */
+    void setTexture(String texture) throws IllegalArgumentException;
 
-    /** Function for adding a number of an item to the stack of the current item.
+    /**
+     * Function for adding a number of an item to the stack of the current item.
      * Note that for non stackable items, this always returns false;
      * @param number: the number of items to add to this items stack
      * @return true or false depending on whether the item was added or not
+     * @throws IllegalArgumentException if number is too large or less than 1
      */
-    boolean addToStack(int number);
+    boolean addToStack(int number) throws IllegalArgumentException;
 
 }
