185d9db4db9b7850119cab78e8ee30278c87f75b
m-crofts <30452973+m-crofts@users.noreply.github.com>
2017-08-28 00:22:59 +1000

    Add comments to MessageType

--- hcg/src/main/java/com/deco2800/hcg/multiplayer/MessageType.java
+++ hcg/src/main/java/com/deco2800/hcg/multiplayer/MessageType.java
@@ -7,5 +7,8 @@ package com.deco2800.hcg.multiplayer;
  *
  */
 public enum MessageType {
-	CONFIRMATION, JOIN, COMMAND, CHAT
+	CONFIRMATION, // sent after receiving message
+	JOIN,         // sent from client when joining
+	COMMAND,      // not used (yet)
+	CHAT          // sent when a peer enters a chat message
 }
