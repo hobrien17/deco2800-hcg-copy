ff7741162994c2c5aa67383e652fe562c5adb02a
canmila <canmila0@gmail.com>
2017-08-27 18:20:52 +1000

    Revert "add resources"
    This reverts commit a9fd08649e6fb86d3495acae83d2f64532fc05ff.

--- hcg/src/main/java/com/deco2800/hcg/managers/SoundManager.java
+++ hcg/src/main/java/com/deco2800/hcg/managers/SoundManager.java
@@ -15,63 +15,47 @@ import java.util.Map;
  */
 public class SoundManager extends Manager {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SoundManager.class);
-	private Map<String, Sound> soundMap = new HashMap<String, Sound>();
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(SoundManager.class);
+    private Map<String, Sound> soundMap = new HashMap<String, Sound>();
 
-	/**
-	 * Constructor Loads all audio files to memory on startup.
-	 */
-	public SoundManager() {
-		soundMap.put("ree", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/ree1.wav")));
-		soundMap.put("quack", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/quack.wav")));
-		// walking sound effect
-		soundMap.put("water-shallow", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/water-shallow.wav")));
-		soundMap.put("water-deep", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/water-deep.wav")));
-		soundMap.put("sand", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/sand.wav")));
-		soundMap.put("ice", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/ice.wav")));
-		soundMap.put("lava", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/lava.wav")));
-		soundMap.put("sludge", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/sludge.wav")));
-		soundMap.put("spikes", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/spikes.wav")));
-		soundMap.put("ground", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/ground.wav")));
-	}
+    /**
+     * Constructor Loads all audio files to memory on startup.
+     */
+    public SoundManager() {
+        soundMap.put("ree", Gdx.audio
+                .newSound(Gdx.files.internal("resources/sounds/ree1.wav")));
+        soundMap.put("quack", Gdx.audio
+                .newSound(Gdx.files.internal("resources/sounds/quack.wav")));
+        soundMap.put("swimming", Gdx.audio
+                .newSound(Gdx.files.internal("resources/sounds/swimming.wav")));
+    }
 
-	/**
-	 * Play the sound mapped to a given string
-	 */
-	public void playSound(String soundString) {
-		Sound sound = soundMap.get(soundString);
-		if (sound != null) {
-			LOGGER.info("Playing sound effect: " + soundString);
-			sound.play(1f);
-		} else {
-			LOGGER.info("No reference to sound effect: " + soundString);
-		}
-	}
+    /**
+     * Play the sound mapped to a given string
+     */
+    public void playSound(String soundString) {
+        Sound sound = soundMap.get(soundString);
+        if (sound != null) {
+            LOGGER.info("Playing sound effect: " + soundString);
+            sound.play(1f);
+        } else {
+            LOGGER.info("No reference to sound effect: " + soundString);
+        }
+    }
 
-	/**
-	 * Stops playing all instances of this sound.
-	 */
-	public void stopSound(String soundString) {
-		Sound sound = soundMap.get(soundString);
-		if (sound != null) {
-			LOGGER.info("Stop sound effect: " + soundString);
-			sound.stop();
-		} else {
-			LOGGER.info("No reference to sound effect: " + soundString);
-		}
-	}
+    /**
+     * Stops playing all instances of this sound.
+     */
 
-	/**
-	 * continue playing sound is a loop
-	 */
-	public void loopSound(String soundString) {
-		Sound sound = soundMap.get(soundString);
-		if (sound != null) {
-			LOGGER.info("Playing sound effect, looping : " + soundString);
-			sound.loop(1f);
-		} else {
-			LOGGER.info("No reference to sound effect: " + soundString);
-		}
-	}
+    public void stopSound(String soundString) {
+        Sound sound = soundMap.get(soundString);
+        if (sound != null) {
+            LOGGER.info("Stop sound effect: " + soundString);
+            sound.stop();
+        } else {
+            LOGGER.info("No reference to sound effect: " + soundString);
+        }
+    }
 
 }
