8ff110aee209e43f3f9a01d188afa8161619f950
Gareth Booth (gareth001) <2gareth2@gmail.com>
2017-09-01 20:46:09 +1000

    fixed collisions with trees maybe

--- hcg/src/main/java/com/deco2800/hcg/entities/terrain_entities/BasicGreenTree.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/terrain_entities/BasicGreenTree.java
@@ -5,7 +5,7 @@ public class BasicGreenTree extends TerrainEntity {
 
     public BasicGreenTree(float posX, float posY, float posZ) {
         super(posX, posY, posZ, 1.0f, 1.0f, 1.0f);
-        this.setCentered();
+        //this.setCentered();
         this.setTexture("tree_01");
     }
 }
