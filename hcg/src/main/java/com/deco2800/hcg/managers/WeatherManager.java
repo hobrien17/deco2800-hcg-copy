66e1b4634bed5a9bfeb5e82f54580c4ec01d874c
AnnaTruffet <anna_truffet@hotmail.com>
2017-09-10 15:17:02 +1000

    Added getOnEffects() getter method, updated doc strings and hit ctrl-shift-f to auto format

--- hcg/src/main/java/com/deco2800/hcg/managers/WeatherManager.java
+++ hcg/src/main/java/com/deco2800/hcg/managers/WeatherManager.java
@@ -8,89 +8,96 @@ import com.badlogic.gdx.graphics.Texture;
 //import com.deco2800.hcg.entities.Tickable;
 import java.util.*;
 
-
 public class WeatherManager extends Manager implements TickableManager {
 
-    // list of effects to be implemented
-    // none
-    // rain
-    // snow
-    // drought
-    // storm (rain, clouds, lightning)
-    // sandstorm
+	// list of effects to be implemented
+	// none
+	// rain
+	// snow
+	// drought
+	// storm (rain, clouds, lightning)
+	// sandstorm
 
+	// rain: the particleEffect (display) for rain in the game
+	ParticleEffect rain;
 
-    ParticleEffect rain;
-    ArrayList<ParticleEffect> effects;
+	// effects: a list of effects that is currnetly on in game.
+	ArrayList<ParticleEffect> effects;
 
+	/**
+	 * Constructor for weather manager
+	 */
+	public WeatherManager() {
+		effects = new ArrayList<ParticleEffect>();
 
-    /**
-     * Constructor
-     */
-    public WeatherManager() {
-        effects = new ArrayList<ParticleEffect>();
+		// maybe does not belong in constructor
+		rain = new ParticleEffect();
 
+		// set-up for rain
+		rain.load(Gdx.files.internal("resources/particles/2dRain.p"),
+				Gdx.files.internal("resources/particles/"));
+		rain.getEmitters().first().setPosition(Gdx.graphics.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2);
+	}
 
-        // maybe does not belong in constructor
-        rain = new ParticleEffect();
+	/**
+	 * Turns on a particle effect,
+	 * 
+	 * If this.getOnEffects() contains effect, nothing will happen
+	 * 
+	 * @param effect
+	 *            the effect that will to be started
+	 * 
+	 * @require effect != null
+	 * 
+	 * @ensure this.getOnEffects() contains effect
+	 */
+	public void startEffect(ParticleEffect effect) {
+		effects.add(effect);
+		effect.start();
+	}
 
-        // set-up for rain
-        rain.load(Gdx.files.internal("resources/particles/2dRain.p"),
-                Gdx.files.internal("resources/particles/"));
-        rain.getEmitters().first().setPosition(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
-    }
+	/**
+	 * Turns off a particle effect, if getOnEffects() does not contain effect,
+	 * nothing will happen
+	 * 
+	 * @ensure this.getOnEffects() does not contain effect
+	 */
+	public void stopEffect(ParticleEffect effect) {
+		if (effects.contains(effect)) {
+			effects.remove(effect);
+		}
+	}
 
-    /**
-     * Turns on a particle effect
-     */
-    public void startEffect(ParticleEffect effect) {
-        effects.add(effect);
-        effect.start();
-    }
+	/**
+	 * getter method for all current effects turned on.
+	 * 
+	 * Please note that this is a deep copy of the effects on list
+	 * 
+	 * @return effects: all current effects turned on in the game
+	 */
+	public ArrayList<ParticleEffect> getOnEffects() {
+		return new ArrayList<ParticleEffect>(effects);
+	}
 
-    /**
-     * Turns off a particle effect
-     */
-    public void stopEffect(ParticleEffect effect) {
-        effects.remove(effect);
-    }
+	/**
+	 * Handles incrementing time on tick event.
+	 *
+	 * @param gameTickCount
+	 *            of all game ticks so far.
+	 */
+	public void onTick(long gameTickCount) {
+		for (ParticleEffect ef : effects) {
+			// this was the sample code for updating the particle effects, Ash
+			// to implement properly
+			// rain.update(Gdx.graphics.getDeltaTime());
+			// rain.draw();
 
-    /**
-     * Handles incrementing time on tick event.
-     *
-     * @param gameTickCount
-     *            of all game ticks so far.
-     */
-    public void onTick(long gameTickCount) {
-        for (ParticleEffect ef: effects) {
-            // this was the sample code for updating the particle effects, Ash to implement properly
-            // rain.update(Gdx.graphics.getDeltaTime());
-            // rain.draw();
-
-            // reset animation if completed
-            if (ef.isComplete()) {
-                ef.reset();
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			// reset animation if completed
+			if (ef.isComplete()) {
+				ef.reset();
+			}
+		}
+	}
 
 }
