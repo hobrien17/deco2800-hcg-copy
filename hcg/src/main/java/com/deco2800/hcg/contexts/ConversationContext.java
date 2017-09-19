bce2de9717e0e0d40c645c6a96daa5f12edfe14d
Richy McGregor <Deepfriedice@gmail.com>
2017-09-19 22:21:43 +1000

    Progress on ConversationContext (#47)

--- hcg/src/main/java/com/deco2800/hcg/contexts/ConversationContext.java
+++ hcg/src/main/java/com/deco2800/hcg/contexts/ConversationContext.java
@@ -1,20 +1,10 @@
 package com.deco2800.hcg.contexts;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Cell;
-import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.deco2800.hcg.conversation.Conversation;
 import com.deco2800.hcg.conversation.ConversationNode;
 import com.deco2800.hcg.conversation.ConversationOption;
@@ -25,49 +15,51 @@ import com.deco2800.hcg.managers.TextureManager;
 public class ConversationContext extends UIContext {
     private Table table;
     private Label nodeText;
-    private List<ConversationOption> nodeOptions;
     private HorizontalGroup buttons;
-    private TextButtonStyle style;
-	
-	public ConversationContext(Conversation x){
+    private Skin skin;
+
+	public ConversationContext(Conversation conversation){
 		super();
+
 		GameManager gameManager = GameManager.get();
 		ContextManager contextManager = (ContextManager)
 	            gameManager.getManager(ContextManager.class);
 		TextureManager textureManager = (TextureManager) 
 				gameManager.getManager(TextureManager.class);
 
-		Skin skin = new Skin(Gdx.files.internal("resources/ui/uiskin.json"));
-		
-		nodeOptions = new ArrayList<ConversationOption>();
-		style = new TextButtonStyle();
-		
+		skin = new Skin(Gdx.files.internal("resources/ui/uiskin.json"));
 		table = new Table();
+		table.setFillParent(true);
+		nodeText = new Label("", skin);
 		buttons = new HorizontalGroup();
 		table.add(nodeText);
+		table.row();
 		table.add(buttons);
 		stage.addActor(table);
-		
-		for(int i = 0; i< buttons.getChildren().size; i++){
-			int index = i;
-			buttons.getChildren().get(index).addListener(new ChangeListener() {
-				@Override
-				public void changed(ChangeEvent event, Actor actor) {
-					nodeOptions.get(index).activate();
-				}
-			});
-		}
+
+		table.debug(); //DEBUG
+
 	}
 
     public void displayNode(ConversationNode node) {
-    	this.nodeText.setText(node.getNodeText());
-    	nodeOptions.clear();
-    	
-    	for(int i = 0; i<node.getOptions().size(); i++){
-    		String optionText = node.getOptions().get(i).getOptionText();
-    		buttons.addActor(new TextButton(optionText, style));
-    		nodeOptions.add(node.getOptions().get(i));
-    	}
+
+		// Clear old buttons & replace text
+		System.err.println(node.getNodeText()); //DEBUG
+    	nodeText.setText(node.getNodeText());
+		buttons.clearChildren();
+
+		// Add new buttons
+		for (ConversationOption option : node.getOptions()) {
+			System.err.println(" * " + option.getOptionText()); //DEBUG
+			TextButton button = new TextButton(option.getOptionText(), skin);
+			button.addListener(new ChangeListener() {
+				@Override
+				public void changed(ChangeEvent event, Actor actor) {
+					option.activate();
+				}
+			});
+			buttons.addActor(button);
+		}
 
     } 
     	
