d56aa94368e3c0a00139732e743d01b93eac5584
Henry O'Brien <h.obrien17@hotmail.com>
2017-09-23 15:09:07 +1000

    More tests... and changed turret names

--- hcg/src/test/java/com/deco2800/hcg/turrets/TurretTest.java
+++ hcg/src/test/java/com/deco2800/hcg/turrets/TurretTest.java
@@ -313,18 +313,22 @@ public class TurretTest {
 		assertEquals("An empty corpse should have the empty corpse sprite", "corpse", corpse.getTexture());
 		corpse.plantInside(new Seed(Seed.Type.SUNFLOWER));
 		assertEquals("The corpse should have the sunflower sprite", "sunflower_corpse", corpse.getTexture());
+		assertEquals("SUNFLOWER", corpse.getTurret().getName().toUpperCase());
 		
 		corpse = new BasicCorpse(CORPSE_X, CORPSE_Y, 0);
 		corpse.plantInside(new Seed(Seed.Type.WATER));
 		assertEquals("The corpse should have the water sprite", "water_corpse", corpse.getTexture());
+		assertEquals("LILY", corpse.getTurret().getName().toUpperCase());
 		
 		corpse = new BasicCorpse(CORPSE_X, CORPSE_Y, 0);
 		corpse.plantInside(new Seed(Seed.Type.GRASS));
 		assertEquals("The corpse should have the grass sprite", "grass_corpse", corpse.getTexture());
+		assertEquals("GRASS", corpse.getTurret().getName().toUpperCase());
 		
 		corpse = new BasicCorpse(CORPSE_X, CORPSE_Y, 0);
 		corpse.plantInside(new Seed(Seed.Type.FIRE));
 		assertEquals("The corpse should have the fire sprite", "fire_corpse", corpse.getTexture());
+		assertEquals("INFERNO", corpse.getTurret().getName().toUpperCase());
 		
 		corpse = new BasicCorpse(CORPSE_X, CORPSE_Y, 0);
 		corpse.plantInside(new Seed(Seed.Type.EXPLOSIVE));
@@ -335,6 +339,7 @@ public class TurretTest {
 		assertEquals("The corpse should have the second cactus sprite", "cactus_corpse_02", corpse.getTexture());
 		corpse.getTurret().update(sw, 3);
 		assertEquals("The corpse should have the third cactus sprite", "cactus_corpse_03", corpse.getTexture());
+		assertEquals("CACTUS", corpse.getTurret().getName().toUpperCase());
 		
 		corpse = new BasicCorpse(CORPSE_X, CORPSE_Y, 0);
 		corpse.plantInside(new Seed(Seed.Type.ICE));
@@ -345,6 +350,7 @@ public class TurretTest {
 		assertEquals("The corpse should have the second ice sprite",  "ice_corpse_02", corpse.getTexture());
 		corpse.getTurret().update(sw, 3);
 		assertEquals("The corpse should have the third ice sprite", "ice_corpse_03", corpse.getTexture());
+		assertEquals("ICE", corpse.getTurret().getName().toUpperCase());
 	}
 	
 	
