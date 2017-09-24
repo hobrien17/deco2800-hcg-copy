d5efd8771ed2d34c46f9b1dc06b716bc9673b1ef
Grace Rehn <grace_rehn@hotmail.com>
2017-09-24 12:06:19 +1000

    fixed enemy loot test

--- hcg/src/test/java/com/deco2800/hcg/entities/EnemyTest.java
+++ hcg/src/test/java/com/deco2800/hcg/entities/EnemyTest.java
@@ -59,11 +59,11 @@ public class EnemyTest {
         assertThat("Loot rarity not valid.", enemy.checkLootRarity(), is(equalTo(true)));
         assertThat("Basic enemy should only have 1 type of loot.", enemy.getLoot().length,
                 is(equalTo(1)));
-        //assertThat("Basic Enemy only has 1 drop.", enemy.loot().length, is(equalTo(1)));
+        assertThat("Basic Enemy only has 1 drop.", enemy.loot().length, is(equalTo(1)));
         Map<String, Double> expectedRarity = new HashMap<>();
         expectedRarity.put("gardening_seed", 1.0);
-        //assertThat("Loot rarity should only have sunflower seed", enemy.getRarity(), is(equalTo(expectedRarity)));
-        //assertThat("Item should be sunflower seed", enemy.randItem(), is(equalTo("gardening_seed")));
+        assertThat("Loot rarity should only have sunflower seed", enemy.getRarity(), is(equalTo(expectedRarity)));
+        assertThat("Item should be sunflower seed", enemy.randItem(), is(equalTo("gardening_seed")));
     }
     
     @Test (expected = IllegalArgumentException.class)
