b3f3b5d0f566c1dd64fbe131fa424d3e3a1d9f87
Yuki Nakazawa <yuxie@hotmail.co.jp>
2017-09-24 22:01:00 +1000

    Added the explosion bullet. Added comments

--- hcg/src/main/java/com/deco2800/hcg/entities/bullets/FireBullet.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/bullets/FireBullet.java
@@ -10,83 +10,103 @@ import com.deco2800.hcg.util.WorldUtil;
 
 /**
  * Fire bullet
- * Kills the enemy it hits, and sets nearby enemies on fire
- * Since effects are not fully implemented, this bullet doesn't work at the moment
- * 
+ * Sets the enemy on fire and deals damage over time
+ *
+ * Omae wa mou shinde-iru
+ *
  * @author Henry O'Brien
  *
  */
 public class FireBullet extends Bullet {
 
+	/**
+	 * Creates a new Bullet at the given position with the given direction.
+	 *
+	 * @param posX
+	 *            the x position of the bullet
+	 * @param posY
+	 *            the y position of the bullet
+	 * @param posZ
+	 *            the z position of the bullet
+	 * @param xd
+	 *            the y direction for the bullet
+	 * @param yd
+	 *            the x direction for the bullet
+	 * @param user
+	 *            the entity using the bullet
+	 * @param hitCount
+	 *            the total number of enemies that can be hit
+	 */
 	public FireBullet(float posX, float posY, float posZ, float xd, float yd,
-					 AbstractEntity user, int hitCount) {
+					  AbstractEntity user, int hitCount) {
 		super(posX, posY, posZ, xd, yd, posZ,
 				user, hitCount);
 		this.setTexture("battle_seed_red");
 	}
-	
+
 	/**
 	 * Creates a new firebullet moving towards the specified location
-	 * 
+	 *
 	 * @param posX
-	 * 			the starting x position
+	 *           the starting x position
 	 * @param posY
-	 * 			the starting y position
+	 *           the starting y position
 	 * @param posZ
-	 * 			the starting z position
+	 *           the starting z position
 	 * @param newX
-	 * 			the target x position
+	 *           the target x position
 	 * @param newY
-	 * 			the target y position
+	 *           the target y position
 	 * @param newZ
-	 * 			the target z position
+	 *           the target z position
 	 * @param user
-	 * 			the entity who shot this bullet
+	 *           the entity who shot this bullet
 	 */
 	public FireBullet(float posX, float posY, float posZ, float newX, float newY, float newZ, AbstractEntity user) {
 		super(posX, posY, posZ, newX, newY, newZ, user, 1);
 		this.setTexture("battle_seed_red");
 	}
-	
+
 	/**
 	 * Creates a new firebullet moving towards a specified location, with given x y and z length
-	 * 
+	 *
 	 * @param posX
-	 * 			the starting x position
+	 *           the starting x position
 	 * @param posY
-	 * 			the starting y position
+	 *           the starting y position
 	 * @param posZ
-	 * 			the starting z position
+	 *           the starting z position
 	 * @param newX
-	 * 			the target x position
+	 *           the target x position
 	 * @param newY
-	 * 			the target y position
+	 *           the target y position
 	 * @param newZ
-	 * 			the target z position
+	 *           the target z position
 	 * @param xLength
-	 * 			the x size of the object
+	 *           the x size of the object
 	 * @param yLength
-	 * 			the y size of the object
+	 *           the y size of the object
 	 * @param zLength
-	 * 			the z size of the object
+	 *           the z size of the object
 	 * @param user
-	 * 			the entity who shot this bullet
+	 *           the entity who shot this bullet
 	 * @param hitCount
-	 * 			the number of entities this object can hit before being destroyed
+	 *           the number of entities this object can hit before being destroyed
 	 */
-	public FireBullet(float posX, float posY, float posZ, float newX, float newY, float newZ, float xLength, 
-			float yLength, float zLength, AbstractEntity user, int hitCount) {
+	public FireBullet(float posX, float posY, float posZ, float newX, float newY, float newZ, float xLength,
+					  float yLength, float zLength, AbstractEntity user, int hitCount) {
 		super(posX, posY, posZ, newX, newY, newZ, xLength, yLength, zLength, user, hitCount);
 		this.setTexture("battle_seed_red");
 	}
-	
+
 	@Override
 	protected void applyEffect(Harmable target) {
 		target.giveEffect(new Effect("Shot", 1, 5, 0, 0, 200, 0));
-//		AbstractEntity entity = (AbstractEntity)target;
-//		List<AbstractEntity> closest = WorldUtil.allEntitiesToPosition(entity.getPosX(), entity.getPosY(), 1, Enemy.class);
-//		for(AbstractEntity close : closest) {
-//			((Enemy)close).giveEffect(new Effect("Fire", 1, 5, 0, 0, 200, 0));
-//		}
+//    AbstractEntity entity = (AbstractEntity)target;
+//    List<AbstractEntity> closest = WorldUtil.allEntitiesToPosition(entity.getPosX(), entity.getPosY(), 1, Enemy.class);
+//    for(AbstractEntity close : closest) {
+//       ((Enemy)close).giveEffect(new Effect("Fire", 1, 5, 0, 0, 200, 0));
+//    }
 	}
 }
+
