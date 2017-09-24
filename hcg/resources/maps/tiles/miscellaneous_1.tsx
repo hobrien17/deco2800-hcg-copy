29acc4ca7da098e2688bdd1324d6137831911787
shadow11675 <s4436755@student.uq.edu.au>
2017-09-24 14:21:18 +1000

    Fixed up miscellaneous tileset

--- hcg/resources/maps/tiles/miscellaneous_1.tsx
+++ hcg/resources/maps/tiles/miscellaneous_1.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="miscellaneous_1" tilewidth="75" tileheight="96" tilecount="15" columns="0">
+<tileset name="miscellaneous_1" tilewidth="75" tileheight="96" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="55" height="32" source="../textures/dry-ground-1.png"/>
@@ -21,29 +21,5 @@
  </tile>
  <tile id="6">
   <image width="55" height="32" source="../textures/stone-1.png"/>
- </tile>
- <tile id="7">
-  <image width="55" height="32" source="../textures/toxic-water-1.png"/>
- </tile>
- <tile id="8">
-  <image width="55" height="32" source="../textures/toxic-water-2.png"/>
- </tile>
- <tile id="9">
-  <image width="55" height="32" source="../textures/toxic-water-3.png"/>
- </tile>
- <tile id="10">
-  <image width="55" height="32" source="../textures/toxic-water-4.png"/>
- </tile>
- <tile id="11">
-  <image width="55" height="32" source="../textures/water-1.png"/>
- </tile>
- <tile id="12">
-  <image width="55" height="32" source="../textures/water-2.png"/>
- </tile>
- <tile id="13">
-  <image width="55" height="32" source="../textures/water-3.png"/>
- </tile>
- <tile id="14">
-  <image width="55" height="32" source="../textures/water-4.png"/>
  </tile>
 </tileset>
