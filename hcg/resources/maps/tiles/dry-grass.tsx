c54ff3245c4f0ef8ca60a08710f145830692b4e6
shadow11675 <s4436755@student.uq.edu.au>
2017-09-25 01:23:51 +1000

    Fixed up urban_wasteland map and sprites.

--- hcg/resources/maps/tiles/dry-grass.tsx
+++ hcg/resources/maps/tiles/dry-grass.tsx
@@ -1,13 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="dry-grass" tilewidth="55" tileheight="32" tilecount="3" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="55" height="32" source="../textures/dry-ground-1.png"/>
- </tile>
- <tile id="1">
-  <image width="55" height="32" source="../textures/sparse-dry-grass-1.png"/>
- </tile>
- <tile id="2">
-  <image width="55" height="32" source="../textures/sparse-dry-grass-2.png"/>
- </tile>
+<tileset name="dried-grass" tilewidth="55" tileheight="32" tilecount="3" columns="3">
+ <image source="../textures/dried-grass.png" width="165" height="32"/>
 </tileset>
