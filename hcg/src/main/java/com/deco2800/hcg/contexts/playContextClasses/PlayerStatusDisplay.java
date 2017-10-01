b23ae6accccbc9d11cb947b69ef20489e1411986
AnnaTruffet <anna_truffet@hotmail.com>
2017-10-01 20:32:19 +1000

    Define a constant instead of duplicating literal #58

--- hcg/src/main/java/com/deco2800/hcg/contexts/playContextClasses/PlayerStatusDisplay.java
+++ hcg/src/main/java/com/deco2800/hcg/contexts/playContextClasses/PlayerStatusDisplay.java
@@ -25,9 +25,13 @@ public class PlayerStatusDisplay extends Group {
     private Label playerLevel;
     private float posY;
     private float posX;
-
+    
+    private String staminaBarLoction;
+    
     public PlayerStatusDisplay() {
         super();
+        staminaBarLoction = "resources/ui/player_status_hud/health_bar_pixel.png";
+        
         /*adding GameManager and obtaining player class*/
         gameManager = GameManager.get();
         PlayerManager playerManager = (PlayerManager) gameManager.getManager(PlayerManager.class);
@@ -39,9 +43,9 @@ public class PlayerStatusDisplay extends Group {
         /* Add the player's health and stamina display*/
         playerImage = new Image(new Texture(player.getDisplayImage()));
         playerBorder = new Image(new Texture(Gdx.files.internal("resources/ui/player_status_hud/player_normal_border.png")));
-        staminaBar = new Image(new Texture(Gdx.files.internal("resources/ui/player_status_hud/health_bar_pixel.png")));
-        healthBar = new Image(new Texture(Gdx.files.internal("resources/ui/player_status_hud/health_bar_pixel.png")));
-        healthBarShadow = new Image(new Texture(Gdx.files.internal("resources/ui/player_status_hud/health_bar_pixel.png")));
+        staminaBar = new Image(new Texture(Gdx.files.internal(staminaBarLoction)));
+        healthBar = new Image(new Texture(Gdx.files.internal(staminaBarLoction)));
+        healthBarShadow = new Image(new Texture(Gdx.files.internal(staminaBarLoction)));
         playerHealth = new Label(player.getHealthCur() + " / " + player.getHealthMax(), skin);
         playerLevel = new Label("Lv: " + player.getLevel(), skin);
 
