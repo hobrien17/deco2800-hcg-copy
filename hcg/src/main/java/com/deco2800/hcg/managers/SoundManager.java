a521da5b4d050d05889565b52baafcb9002fd0ac
Steven Khoupongsy <stkontheweb@gmail.com>
2017-10-14 01:08:03 +1000

    Parital bug fix: Weather sound no longer plays when player finishes level and returns ot world map. However, if player leaves the level by pressing escape, issue remains. #80

--- hcg/src/main/java/com/deco2800/hcg/managers/SoundManager.java
+++ hcg/src/main/java/com/deco2800/hcg/managers/SoundManager.java
@@ -4,10 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -24,12 +24,15 @@ public class SoundManager extends Manager {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SoundManager.class);
 	private Map<String, Sound> soundMap = new HashMap<String, Sound>();
-	
+
 	ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 	ScheduledFuture<?> randomLoop;
-	
+
+	ArrayList<Sound> currentlyLooping = new ArrayList<Sound>();
+
 	// String Constants
 	private static final String NO_REF = "No reference to sound effect: ";
+
 	/**
 	 * Constructor Loads all audio files to memory on startup.
 	 */
@@ -51,34 +54,50 @@ public class SoundManager extends Manager {
 			soundMap.put("hit", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/hit.wav")));
 
 			// Gun sounds
-			soundMap.put("gun-rifle-shoot", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-rifle-shoot.wav")));
-			soundMap.put("gun-shotgun-shoot", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-shotgun-shoot.wav")));
-			soundMap.put("gun-stargun-shoot", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-stargun-shoot.wav")));
-			soundMap.put("gun-grenadelauncher-shoot", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-grenadelauncher-shoot.wav")));
-			soundMap.put("bullet-grenade-explode", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/bullet-grenade-explode.wav")));
+			soundMap.put("gun-rifle-shoot",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-rifle-shoot.wav")));
+			soundMap.put("gun-shotgun-shoot",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-shotgun-shoot.wav")));
+			soundMap.put("gun-stargun-shoot",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-stargun-shoot.wav")));
+			soundMap.put("gun-grenadelauncher-shoot",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/gun-grenadelauncher-shoot.wav")));
+			soundMap.put("bullet-grenade-explode",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/bullet-grenade-explode.wav")));
 
 			// For weather Effects
-			soundMap.put("weatherRain", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/environmental/rain-ambient-hardsurface-shortloop.wav")));
-			soundMap.put("weatherSandStorm", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/environmental/darude-sandstorm-shortloop.wav")));
-			soundMap.put("weatherSnow", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/environmental/wind-ambient-semirandom-longloop-spooky.wav")));
-			soundMap.put("weatherWind", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/environmental/wind-ambient-semirandom-longloop-spooky.wav")));
-			soundMap.put("weatherDrought", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/environmental/wind-ambient-semirandom-longloop-spooky.wav")));
-			soundMap.put("weatherStorm", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/environmental/rain-ambient-hardsurface-shortloop.wav")));
+			soundMap.put("weatherRain", Gdx.audio.newSound(
+					Gdx.files.internal("resources/sounds/environmental/rain-ambient-hardsurface-shortloop.wav")));
+			soundMap.put("weatherSandStorm", Gdx.audio
+					.newSound(Gdx.files.internal("resources/sounds/environmental/darude-sandstorm-shortloop.wav")));
+			soundMap.put("weatherSnow", Gdx.audio.newSound(
+					Gdx.files.internal("resources/sounds/environmental/wind-ambient-semirandom-longloop-spooky.wav")));
+			soundMap.put("weatherWind", Gdx.audio.newSound(
+					Gdx.files.internal("resources/sounds/environmental/wind-ambient-semirandom-longloop-spooky.wav")));
+			soundMap.put("weatherDrought", Gdx.audio.newSound(
+					Gdx.files.internal("resources/sounds/environmental/wind-ambient-semirandom-longloop-spooky.wav")));
+			soundMap.put("weatherStorm", Gdx.audio.newSound(
+					Gdx.files.internal("resources/sounds/environmental/rain-ambient-hardsurface-shortloop.wav")));
 			soundMap.put("weatherStormSting", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/ree2.wav")));
-			
+
 			// For gardening
-			soundMap.put("plantingPot", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/planting_in_soil_pot.wav")));
+			soundMap.put("plantingPot",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/planting_in_soil_pot.wav")));
 			soundMap.put("bugSpray", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/bugspray.wav")));
 			soundMap.put("explosion", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/explosion.wav")));
-			soundMap.put("fertiliser", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/fertiliser.wav")));
+			soundMap.put("fertiliser",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/fertiliser.wav")));
 			soundMap.put("fireball", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/fireball.wav")));
 			soundMap.put("grass", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/grass.wav")));
 			soundMap.put("iceTurret", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/ice_tree.wav")));
-			soundMap.put("plantingCorpse", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/plant_in_enemies.wav")));
-			soundMap.put("plantingLily", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/plant_waterlilly_in_pot.wav")));
-			soundMap.put("shovelTrowel", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/shovel_or_trowel.wav")));
+			soundMap.put("plantingCorpse",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/plant_in_enemies.wav")));
+			soundMap.put("plantingLily",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/plant_waterlilly_in_pot.wav")));
+			soundMap.put("shovelTrowel",
+					Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/shovel_or_trowel.wav")));
 			soundMap.put("key", Gdx.audio.newSound(Gdx.files.internal("resources/sounds/garden/key.wav")));
-			
+
 		} catch (GdxRuntimeException e) {
 			throw new ResourceLoadException(e);
 		}
@@ -111,7 +130,6 @@ public class SoundManager extends Manager {
 		}
 	}
 
-	
 	/**
 	 * continue playing sound is a loop
 	 */
@@ -124,21 +142,32 @@ public class SoundManager extends Manager {
 			LOGGER.info(NO_REF + soundString);
 		}
 	}
-	
+
 	/**
 	 * special loop sound player for weather effects
 	 */
 	public void ambientLoopSound(String soundString) {
+
+		for (Sound playing : currentlyLooping) {
+			playing.stop();
+		}
+		
+		currentlyLooping.clear();
+
 		Sound sound = soundMap.get(soundString);
-		Sound sting = soundMap.get("weatherStormSting"); //change this to unique sounds
+		Sound sting = soundMap.get("weatherStormSting"); // change this to unique sounds
+
+		currentlyLooping.add(sound);
+		currentlyLooping.add(sting);
+
 		if (sound != null) {
 			LOGGER.info("Playing sound effect, looping : " + soundString);
 			sound.loop(1f);
 
 			Runnable stingSound = new Runnable() {
-			    public void run() {
-			        randomPlaySound(sting);
-			    }
+				public void run() {
+					randomPlaySound(sting);
+				}
 			};
 
 			try {
@@ -147,21 +176,20 @@ public class SoundManager extends Manager {
 				LOGGER.info("No sting playing yet");
 			}
 			randomLoop = executor.scheduleAtFixedRate(stingSound, 0, 4, TimeUnit.SECONDS);
-			
+
 		} else {
 			LOGGER.info(NO_REF + soundString);
 		}
 	}
-	
+
 	/*
-	 *  Helper method that provides a chance to play a sound at a 
-	 *  random interval
-	 *  
+	 * Helper method that provides a chance to play a sound at a random interval
+	 * 
 	 */
 	private void randomPlaySound(Sound sound) {
 		double random = Math.random();
 		if (random > 0.65) {
-			sound.play((float)random); //varies intensity
+			sound.play((float) random); // varies intensity
 		}
 	}
 
