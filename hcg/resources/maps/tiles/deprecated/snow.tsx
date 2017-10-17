e07418da8775ffb8f59f53f190fad9845a97a718
Gareth Booth (gareth001) <2gareth2@gmail.com>
2017-10-17 22:39:21 +1000

    Fixed crash on snow map, added oil tiles, changed first snow map to be a wastleland map to fit with the world biomes, and some minor changes to other worlds.

--- hcg/resources/maps/tiles/deprecated/snow.tsx
+++ hcg/resources/maps/tiles/deprecated/snow.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="snow" tilewidth="55" tileheight="32" tilecount="1" columns="1">
- <image source="../../textures/snow.png" width="56" height="32"/>
+<tileset name="snow" tilewidth="55" tileheight="32" tilecount="14" columns="7">
+ <image source="../../textures/ice_base_tiles.png" width="385" height="64"/>
 </tileset>
