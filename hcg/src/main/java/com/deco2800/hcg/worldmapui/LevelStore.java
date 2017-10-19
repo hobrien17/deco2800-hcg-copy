0142de14fe23ff0fbd595df3f87b42cdbc70aba9
Gareth Booth (gareth001) <2gareth2@gmail.com>
2017-10-19 19:12:13 +1000

    Fixed the broken wasteland map, it works now

--- hcg/src/main/java/com/deco2800/hcg/worldmapui/LevelStore.java
+++ hcg/src/main/java/com/deco2800/hcg/worldmapui/LevelStore.java
@@ -22,9 +22,9 @@ public class LevelStore {
 		
 		// Normal Levels
 		levelList.add(new Level(new World("resources/maps/maps/suburbanWasteland_normal_01.tmx"), 1, 1, 1));
-		//levelList.add(new Level(new World("resources/maps/maps/suburbanWasteland_normal_02.tmx"), 1, 2, 1));
+		levelList.add(new Level(new World("resources/maps/maps/suburbanWasteland_normal_02.tmx"), 1, 2, 1));
 		levelList.add(new Level(new World("resources/maps/maps/suburbanWasteland_normal_03.tmx"), 1, 3, 1));
-	    levelList.add(new Level(new World("resources/maps/maps/suburbanWasteland_normal_04.tmx"), 1, 3, 1));
+	    levelList.add(new Level(new World("resources/maps/maps/suburbanWasteland_normal_04.tmx"), 1, 4, 1));
 		levelList.add(new Level(new World("resources/maps/maps/grass_normal_01.tmx"), 2, 4, 1));
 		levelList.add(new Level(new World("resources/maps/maps/grass_normal_02.tmx"), 2, 4, 1));
 	    levelList.add(new Level(new World("resources/maps/maps/grass_normal_03.tmx"), 2, 4, 1));
