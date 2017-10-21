3471ebb7a4f2b45c7897df7340a079d36eb0ec76
Ryan Lonergan <ryan.lonergan@uq.net.au>
2017-10-21 19:01:38 +1000

    Tutorial02 conversation quest working.

--- hcg/src/main/java/com/deco2800/hcg/managers/ConversationManager.java
+++ hcg/src/main/java/com/deco2800/hcg/managers/ConversationManager.java
@@ -28,6 +28,7 @@ public class ConversationManager extends Manager {
             loadConversation("test_conversation_02", "resources/conversations/test_conversation_02.json");
             loadConversation("james_test", "resources/conversations/james.json");
             loadConversation("tutorial01", "resources/conversations/tutorial01.json");
+            loadConversation("tutorial02", "resources/conversations/tutorial02.json");
         } catch (IOException e) {
             throw new ResourceLoadException(e);
         }
