faabaa14006d1a94f18ff6db3fcf6147b7300dbe
India <india.fallon@gmail.com>
2017-10-22 10:00:16 +1000

    #58 - update try catch statement to preserve original exception

--- hcg/src/main/java/com/deco2800/hcg/worlds/World.java
+++ hcg/src/main/java/com/deco2800/hcg/worlds/World.java
@@ -456,7 +456,7 @@ public class World {
 					try {
 						collisionMap.get(x, y).remove(entity);
 					} catch(IndexOutOfBoundsException ex) {
-						LOGGER.warn("Can't remove entity");
+						LOGGER.warn("Can't remove entity",ex);
 					}
 				}
 			}
