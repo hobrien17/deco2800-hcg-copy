3280879934b4df2a30040fa31111284b87137178
m-crofts <30452973+m-crofts@users.noreply.github.com>
2017-10-23 05:17:07 +1000

    Give starting items to multiplayer characters

--- hcg/src/main/java/com/deco2800/hcg/managers/PlayerManager.java
+++ hcg/src/main/java/com/deco2800/hcg/managers/PlayerManager.java
@@ -3,6 +3,11 @@ package com.deco2800.hcg.managers;
 import java.util.ArrayList;
 
 import com.deco2800.hcg.entities.Player;
+import com.deco2800.hcg.entities.garden_entities.seeds.Seed;
+import com.deco2800.hcg.items.Item;
+import com.deco2800.hcg.items.single.wearable.CottonShirt;
+import com.deco2800.hcg.items.stackable.HealthPotion;
+import com.deco2800.hcg.items.tools.Shovel;
 import com.deco2800.hcg.multiplayer.CharacterMessage;
 import com.deco2800.hcg.worlds.World;
 
@@ -96,6 +101,21 @@ public class PlayerManager extends Manager {
 		player.initialiseNewPlayer(stats[character][0], stats[character][1], stats[character][2],
 			stats[character][3], stats[character][4], stats[character][5], stats[character][6],
 			stats[character][7], 10, character);
+		//TODO: Change this, currently these are just testing items
+        Item test = new CottonShirt(CottonShirt.ShirtColour.BLACK);
+        Item test2 = new CottonShirt(CottonShirt.ShirtColour.GREEN);
+        Item testPotion = new HealthPotion(100);
+        Item startingSeeds = new Seed(Seed.Type.SUNFLOWER);
+        Item shovel = new Shovel();
+        startingSeeds.setStackSize(200);
+        testPotion.setStackSize(4);
+        Item testPotion2 = new HealthPotion(100);
+        player.addItemToInventory(test);
+        player.addItemToInventory(test2);
+        player.addItemToInventory(testPotion);
+        player.addItemToInventory(testPotion2);
+        player.addItemToInventory(startingSeeds);
+        player.addItemToInventory(shovel);
 		return player;
 	}
 
