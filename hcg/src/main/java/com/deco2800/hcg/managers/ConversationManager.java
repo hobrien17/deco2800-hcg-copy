2a23cd1ac0d5e7353af203c8afb7a5a9b1c12c57
Gareth Booth (gareth001) <2gareth2@gmail.com>
2017-10-22 12:31:54 +1000

    Added first 3 tutorial levels from existing levels #79

--- hcg/src/main/java/com/deco2800/hcg/managers/ConversationManager.java
+++ hcg/src/main/java/com/deco2800/hcg/managers/ConversationManager.java
@@ -27,6 +27,9 @@ public class ConversationManager extends Manager {
             loadConversation("test_conversation_01", "resources/conversations/test_conversation_01.json");
             loadConversation("test_conversation_02", "resources/conversations/test_conversation_02.json");
             loadConversation("james_test", "resources/conversations/james.json");
+            loadConversation("tutorialslippery", "resources/conversations/tutorialslippery.json");
+            loadConversation("tutorialwasteland", "resources/conversations/tutorialwasteland.json");
+            loadConversation("tutorialfirst", "resources/conversations/tutorialfirst.json");
         } catch (IOException e) {
             throw new ResourceLoadException(e);
         }
