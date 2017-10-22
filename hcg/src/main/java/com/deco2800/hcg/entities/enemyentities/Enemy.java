c0a60972fb4853cf2ce3f200c626939146ef0f8d
Jingwei WANG <wjw950224@outlook.com>
2017-10-22 12:01:23 +1000

    add tree class

--- hcg/src/main/java/com/deco2800/hcg/entities/enemyentities/Enemy.java
+++ hcg/src/main/java/com/deco2800/hcg/entities/enemyentities/Enemy.java
@@ -717,39 +717,7 @@ public abstract class Enemy extends Character implements Lootable {
         return closestDistance;
     }
 
-    /**
-     *  Logic for Tree
-     *
-     */
-    void tree(){
-        this.setMovementSpeed(0);
-        this.defaultSpeed = 0;
 
-        GameManager.get().getWorld().getWidth();
-        GameManager.get().getWorld().getLength();
-        if ((this.getHealthCur() < this.getHealthMax()) && (this.getHealthCur() > this.getHealthMax()*0.8)){
-            //bottom
-
-            this.setPosX((float) (GameManager.get().getWorld().getWidth() * 0.5));
-            this.setPosY(0);
-        } else if ((this.getHealthCur() < this.getHealthMax()*0.8) && (this.getHealthCur() > this.getHealthMax()*0.6)){
-            //left
-            this.setPosX(0);
-            this.setPosY((float) (GameManager.get().getWorld().getLength() * 0.5));
-        } else if ((this.getHealthCur() < this.getHealthMax()*0.6) && (this.getHealthCur() > this.getHealthMax()*0.4)){
-            //right
-            this.setPosX(GameManager.get().getWorld().getWidth());
-            this.setPosY((float) (GameManager.get().getWorld().getLength() * 0.5));
-        } else if ((this.getHealthCur() < this.getHealthMax()*0.4) && (this.getHealthCur() > this.getHealthMax()*0.2)){
-            //top
-            this.setPosX((float) (GameManager.get().getWorld().getWidth() * 0.5));
-            this.setPosY(GameManager.get().getWorld().getLength());
-        } else {
-            //middle
-            this.setPosX((float) (GameManager.get().getWorld().getWidth() * 0.5));
-            this.setPosY((float) (GameManager.get().getWorld().getLength() * 0.5));
-        }
-    }
     
     @Override
     public boolean equals(Object object) {
